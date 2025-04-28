4ab15aebb7e30a360fe6dd91f8d4ca9e958e8ce6
salo <207671767+arianna238@users.noreply.github.com>
2025-04-28 11:44:05 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,10 @@ public class Main {
         Factory factory = new Factory();
 
         Car car1 = new Car("red", "BMW", 5);
-        Car car2 = new Car("green", "Audi", 3);
+        Car car2 = new Car("blue", "Audi", 3);
         Car car3 = new Car("blue", "VW", 3);
 
         factory.buildCar(car1);
         factory.buildCar(car2);
-
     }
 }
-
